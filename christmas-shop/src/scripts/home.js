46ee4ea7d14ebb738a5b95eb0cf62b41754e6770
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2024-11-13 18:26:05 +1300

    refactor: configue webpack for the project

---
+++ christmas-shop/src/scripts/home.js
@@ -0,0 +1,7 @@
+import '../styles/home.scss';
+
+function calc(a, b) {
+  return a * b;
+}
+
+console.log(calc(2,5));
---
+++ christmas-shop/webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// const pages = ['home'];
+
+module.exports = (env) => {
+  const isDev = env.mode === 'development';
+  const isProd = env.mode === 'production';
+
+  return {
+    mode: env.mode ?? 'development',
+
+    /*entry: pages.reduce((config, page) => {
+      config[page] = `./scripts/${page}.js`;
+      return config;
+    }, {}),*/
+
+    entry: {
+      home: path.resolve(__dirname, 'src', 'scripts', 'home.js'),
+      gift: path.resolve(__dirname, 'src', 'scripts', 'gift.js'),
+    },
+    
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'scripts/[name].js',
+      clean: true,
+      // assetModuleFilename: 'assets/[name][ext]'
+    },
+
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+
+    /* plugins: [].concat(
+      pages.map(
+        (page) =>
+          new HtmlWebpackPlugin({
+            inject: true,
+            template: `pages/${page}.html`,
+            filename: `pages/${page}.html`,
+            chunks: [page],
+          })
+      )
+    ), */
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: path.resolve(__dirname, 'src', 'pages', 'home.html'),
+        filename: 'index.html',
+        chunks: ['home'],
+      }),
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: path.resolve(__dirname, 'src', 'pages', 'gift.html'),
+        filename: 'gift.html',
+        chunks: ['gift'],
+      }),
+      isProd && new MiniCssExtractPlugin({
+        filename: 'styles/[name].css'
+      })
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.(c|sc)ss$/i,
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"],
+        },
+        /*{
+          test: /\.(jpe?g|png|webp|gif|svg)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name][ext]'
+          }
+        },*/
+      ],
+    },
+
+    devtool: isDev ? 'inline-source-map' : false,
+    devServer: isDev ? {
+      open: true,
+    } : undefined,
+  }  
+};
