eb5c92d10abe433fdfae6635d82c741a0d18b6cc
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2024-12-10 14:11:14 +1300

    feat: complete header burger menu implementation

--- christmas-shop/src/scripts/gift.js
+++ christmas-shop/src/scripts/gift.js
@@ -1,1 +1,8 @@
 import '../styles/gift.scss';
+import { handleBurgerButtonClick, handleMenuLinkClick, handleOpenMenuResize } from './components/burger';
+
+window.onload = function() {
+  handleBurgerButtonClick();
+  handleMenuLinkClick();
+  handleOpenMenuResize();
+}
