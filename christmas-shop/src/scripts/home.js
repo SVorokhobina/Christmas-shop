cac7828ce1fa5354bdcc40193aaa52051008b9f0
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2025-01-16 21:28:04 +1300

    feat: implement New Year timer; add scroll inside burger menu

--- christmas-shop/src/scripts/home.js
+++ christmas-shop/src/scripts/home.js
@@ -1,8 +1,16 @@
 import '../styles/home.scss';
 import { handleBurgerButtonClick, handleMenuLinkClick, handleOpenMenuResize } from './components/burger';
+import { setSliderYear, setTimer } from './components/timer';
 
 window.onload = function() {
+  // burger menu
   handleBurgerButtonClick();
   handleMenuLinkClick();
   handleOpenMenuResize();
+
+  // slider
+  setSliderYear();
+
+  // timer
+  setTimer();
 }
